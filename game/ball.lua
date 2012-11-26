f031695882127b01d6a638edc92113ecac7d38a5
quan <conduongmua1206@yahoo.com>
2012-11-26 14:27:10 +0700

    quanvn: edit ball.lua set hit to controller

--- game/ball.lua
+++ game/ball.lua
@@ -22,7 +22,8 @@ local function onTouch(event)
 		if ball.state == state.throwing then
 			--danh bong khi bong duoc nem
 			print "danh cai nao"
-			ball:applyForce( 200, -100, ball.x, ball.y )
+			controller.hitBall(200,-100,ball.x,ball.y)
+			--ball:applyForce( 200, -100, ball.x, ball.y )
 			ball.state = state.flying
 		else
 			-- chua nem danh the deo nao duoc
